ffacafed458fad55667ce51d0cbecf77059d40cd
zhenyameow <jenny.momsen@yandex.ru>
2020-11-08 21:25:52 +0300

    скорректированный код

---
+++ SomeProject/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SomeProject
+//
+//  Created by Nikita Petrenkov on 02/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    let sample = "пример"
+    print(sample)
+    
+        func someMethod() {
+            
+        }
+    if false {
+        print("Будет ли выполнена эта строка?") // эта строка никогда не будет выполнена
+        someMethod()
+    }
+}
+    
+}
+
